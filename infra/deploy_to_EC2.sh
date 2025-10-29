e2797120457c98a065c8fbd746c06c966d72e7a7
buyamm <truongcongly139@gmail.com>
2025-10-29 20:14:28 +0700

    feat: Implementing CI-CD on local

--- infra/deploy_to_EC2.sh
+++ infra/deploy_to_EC2.sh
@@ -3,7 +3,9 @@ set -e
 
 echo "🚀 Starting deployment on EC2..."
 
-cd /home/ubuntu/app   # <== Thư mục chứa docker-compose.prod.yml
+# cd /home/ubuntu/app   # <== Thư mục chứa docker-compose.prod.yml
+
+cd ./
 
 echo "🧹 Stopping old containers..."
 docker compose -f docker-compose.prod.yml down
